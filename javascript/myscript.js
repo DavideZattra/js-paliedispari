18e3a1f7b092651c9e3713e85ffb4977bb581689
DavideZattra <d.zattra@gmail.com>
2021-08-31 15:57:04 +0200

    initialized js-paliedispari

---
+++ javascript/myscript.js
@@ -0,0 +1,35 @@
+//  A-Palidroma
+//      Chiedere all’utente di inserire una parola
+//      Creare una funzione per capire se la parola inserita è palindroma.
+
+//  B-Pari e Dispari
+//      L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+//      Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+//      Sommiamo i due numeri
+//      Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+//      Dichiariamo chi ha vinto.
+
+
+// A
+
+let userWord = prompt('inserisci una parola per sapere se è palindroma');
+
+function palindromeDetector(parola){
+
+    for (i = 0; i < parola.length; i++){
+
+        if (parola.charAt(i) === parola.charAt(str.length-i++)){
+
+            flag = true
+
+        } else {
+
+            flag = false
+
+        }
+        
+
+    }
+}
+
+if (palindromeDetector(userword))
